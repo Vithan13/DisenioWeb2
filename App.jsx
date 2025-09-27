d866f62eae53ff648bddf25a8b0fa14b14248b1e
Vidal Cristhian <dayamcristhian.v.m@gmail.com>
2025-09-26 20:17:57 -0400

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import UsuarioCRUD from './components/UsuarioCRUD'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <UsuarioCRUD />
+    </div>
+  )
+}
+
+export default App
